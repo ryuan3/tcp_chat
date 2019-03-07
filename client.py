ef618b4d52362c099e3ce84f13921634cf3f3702
ryuan3 <ryuan3@usfca.edu>
2019-03-07 19:55:15 +0800

    adjust the server.py, move some code into functions

--- client.py
+++ client.py
@@ -7,9 +7,18 @@ print('host = %s' %host)
 port = 12345
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-
-print(s.recv(1024).decode())
-s.send('Lisa'.encode())
-print(s.recv(1024).decode())
-s.send('exit'.encode())
+print("Linked! Please input your name:")
+name = input()
+s.send(("%s,0" %name).encode())
+info = ""
+while info != "exit":
+    print("From Others: "+info)
+    send_mes = input()
+    print("send_mes = %s" %send_mes)
+    s.send(send_mes.encode())
+    if send_mes == "exit":
+        print("exit")
+        break
+    info = s.recv(1024).decode()
 s.close()
+
--- server.py
+++ server.py
@@ -4,27 +4,37 @@ import socket
 import time
 import threading
 
-def tcplink(sock, addr):
+def newConnection(sock, addr):
     print('Accept new connection from %s:%s' % addr)
     sock.send('Welcome!'.encode())
     while True:
-        data = sock.recv(1024).decode()
+        raw_data = sock.recv(1024).decode()
         time.sleep(1)
-        if data == 'exit' or not data:
+        if raw_data == 'exit' or not raw_data:
+            sock.close()
+            print('Connection from %s:%s closed.' %addr)
             break
-        sock.send(('Hello, %s!' % data).encode())
-    sock.close()
-    print('Connection from %s:%s closed.' %addr)
+        data = raw_data.split(',')
+        if data[0] == '0':
+            sock.send(('Hello, %s!' % data[1]).encode())
+        if data[0] == '1':
+            sock.send(('%s: %s' %data[1] %data[2]).encode())
+#    sock.close()
+#    print('Connection from %s:%s closed.' %addr)
 
-#host = socket.gethostname()
-host = '127.0.0.1'
-print('host= %s'% host)
-port = 12345
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(5)
+def serverInit():
+    #host = socket.gethostname()
+    host = '127.0.0.1'
+    print('host= %s'% host)
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(5)
+
+
+
 while True:
     sock, addr = s.accept()
-    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t = threading.Thread(target=newConnection, args=(sock, addr))
     t.start()
 
